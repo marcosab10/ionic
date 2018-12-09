2dec50fce3909faf5027eba44762ea6404068cdd
Marcos Antonio Bittencourt Faria <mbfaria@brq.com>
2018-12-09 00:49:12 -0200

    Projeto Angular

---
+++ jedi-academy/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jad-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'jedi-academy';
+}
