ebeba66f4a357b9331f5fd93fe1ab821c4d73762
Marcos Antonio Bittencourt Faria <mbfaria@brq.com>
2018-12-09 21:08:37 -0200

    Diretivas e Eventos

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import {Student} from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jedi-academy';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Han Solo', isJedi: false},
+    {name: 'Leia', isJedi: false}
+  ]
+
 }
