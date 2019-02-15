912c55ec553d5531190574ae98919c989fb6a703
marcosab10 <marcosab10@yahoo.com.br>
2019-02-15 19:02:57 -0200

    Versao Inicial App Camera

---
+++ ionic3_recursos_nativos/AppCamera/src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+
+  constructor(private camera: Camera) {
+
+  }
+
+  abreCamera(){
+
+    this.camera.getPicture().then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+
+  }
+
+
+}
