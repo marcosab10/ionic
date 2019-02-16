4e39a9d79186b1367d9c6096949ce3e4d1b13517
marcosab10 <marcosab10@yahoo.com.br>
2019-02-15 23:41:38 -0200

    Camera

--- ionic3_recursos_nativos/AppCamera/src/app/home/home.page.ts
+++ ionic3_recursos_nativos/AppCamera/src/app/home/home.page.ts
@@ -7,18 +7,25 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  base64Image:string;
 
   constructor(private camera: Camera) {
 
   }
 
   abreCamera(){
+    let  config: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
 
-    this.camera.getPicture().then((imageData) => {
+
+    this.camera.getPicture(config).then((imageData) => {
      // imageData is either a base64 encoded string or a file URI
      // If it's base64 (DATA_URL):
-     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
      // Handle error
     });
