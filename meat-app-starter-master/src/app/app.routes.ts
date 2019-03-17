25c572ad62447dd7a794205cc06cb900bbdae045
marcosab10 <marcosab10@yahoo.com.br>
2019-03-16 22:54:05 -0300

    Componentes detalhe

--- meat-app-starter-master/src/app/app.routes.ts
+++ meat-app-starter-master/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router'
 import { HomeComponent } from "app/home/home.component";
 import { AboutComponent } from "app/about/about.component";
 import { RestaurantsComponent } from "app/restaurants/restaurants.component";
+import { RestaurantDetailComponent } from "app/restaurant-detail/restaurant-detail.component";
 
 
 export const ROUTES: Routes = [
  {path: '', component: HomeComponent },
  {path: 'about', component: AboutComponent },
- {path: 'restaurants', component: RestaurantsComponent }
+ {path: 'restaurants', component: RestaurantsComponent },
+ {path: 'restaurants/:id', component: RestaurantDetailComponent }
 ]
