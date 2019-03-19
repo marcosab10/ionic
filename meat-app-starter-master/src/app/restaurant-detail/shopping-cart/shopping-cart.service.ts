2ea34f9c34bd467f119ab1734f81c7315632edbd
Marcos Antonio Bittencourt Faria <mbfaria@brq.com>
2019-03-19 18:59:47 -0300

    Detalhe pontuacao

---
+++ meat-app-starter-master/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -0,0 +1,34 @@
+import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
+
+export class ShoppingCartService {
+  items: CartItem[] = []
+  clear(){
+    this.items = []
+  }
+
+  addItem(item:MenuItem){
+    let foundItem = this.items.find((mItem) => mItem.menuItem.id == item.id )
+
+    if(foundItem){
+      foundItem.quantity = foundItem.quantity + 1
+    } else {
+      this.items.push(new CartItem(item))
+    }
+
+  }
+
+//localiza o indice e remove um elemento que é o proprio indice quando se passa 1
+  removeItem(item:CartItem){
+    this.items.splice(this.items.indexOf(item), 1)
+  }
+
+// Usa o map para transformar o array de item em array de valor
+// depois usa o reduce para somar todos os valores iniciando de 0 a soma
+  total():number {
+    return this.items
+    .map(item => item.value())
+    .reduce((prev, value) => prev + value, 0)
+  }
+
+}
