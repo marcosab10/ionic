1f8079fffa8529a03a987c17bf75a755538327be
marcosab10 <marcosab10@yahoo.com.br>
2019-04-20 21:15:10 -0300

    Reactive Forms

--- reactive_masm/src/app/order/order.component.ts
+++ reactive_masm/src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
+
 import { Router } from '@angular/router'
 
 import { RadioOption } from "app/shared/radio/radio-option.model";
@@ -12,6 +14,12 @@ import { Order, OrderItem } from "app/order/order.model";
 })
 export class OrderComponent implements OnInit {
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern = /^[0-9]*$/
+
+  orderForm: FormGroup
+
   //numa aplicação real estaria trazendo esse valor do backend
   delivery: number = 8
 
@@ -24,9 +32,31 @@ export class OrderComponent implements OnInit {
   ]
 
   constructor(private orderService: OrderService,
-              private router: Router) { }
+              private router: Router,
+            private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required])
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation){
+      return undefined
+    }
+    if(email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true}
+    }
+    return undefined
   }
 
   itemsValue(): number {
--- reactive_masm/src/app/shared/input/input.component.ts
+++ reactive_masm/src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
-import { NgModel } from '@angular/forms'
+import { NgModel, FormControlName } from '@angular/forms'
 
 @Component({
   selector: 'mt-input-container',
@@ -13,6 +13,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   input: any
 
   @ContentChild(NgModel) model: NgModel
+  @ContentChild(FormControlName) control: FormControlName
 
   constructor() { }
 
@@ -21,9 +22,10 @@ export class InputComponent implements OnInit, AfterContentInit {
 
   //Executado assim que o conteudo que ira ficar no lugar de ngContent for definido
   ngAfterContentInit(){
-    this.input = this.model
+    // se a diretiva NgModel nao estiver disponivel vai pegar a FormControlName
+    this.input = this.model || this.control
     if(this.input == undefined){
-      throw new Error('Esse componente precisa ser usado com uma diretiva ngModel')
+      throw new Error('Esse componente precisa ser usado com uma diretiva ngModel ou formControlName')
     }
   }
 
